f81ba97c57d927d3156e7d772001e3aca3f29fd6
zitaluk630 <92528317+zitaluk630@users.noreply.github.com>
2021-10-15 00:36:37 +0800

    Update config.php

--- INFS3208/application/config/config.php
+++ INFS3208/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://infs3202-1e5f9ba8.uqcloud.net/INFS3208/';
+$config['base_url'] = 'http://34.68.132.89:8080/INFS3208/';
 
 /*
 |--------------------------------------------------------------------------
